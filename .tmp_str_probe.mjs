57c7c0bd7aa1d90919db48754d984b5d7b957a06
vmoranv <1761886648@qq.com>
2026-08-04 22:51:54 +0800

    chore: remove agent temp probe file

--- .tmp_str_probe.mjs
+++
@@ -1,4 +0,0 @@
-const path = 'C:\\Windows\\System32\\svchost.exe'.toLowerCase();
-console.log(JSON.stringify(path));
-console.log('has windows seg:', path.includes('\\windows\\'));
-console.log('has svchost:', path.includes('svchost.exe'));
